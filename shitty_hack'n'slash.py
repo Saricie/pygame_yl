d514bf8b7cc1559e779f75d186eafd44feef2a53
Saricie <sariciegh@gmail.com>
2023-01-05 19:31:02 +0500

    Добавлены спрайты

--- shitty_hack'n'slash.py
+++ shitty_hack'n'slash.py
@@ -8,21 +8,16 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(tiles_group, all_sprites)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+            TILE_WIDTH * pos_x, TILE_HEIGHT * pos_y)
 
 
 class Character(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows):
         super().__init__(all_sprites)
-
-        self.frames = []
-        self.sheet = sheet
-
-        self.cut_sheet(self.sheet, columns, rows)
-        self.cur_frame = 0
+        self.change_animation(load_image("mario.png"), 1, 1)
         self.image = self.frames[self.cur_frame]
-
         self.mask = pygame.mask.from_surface(self.image)
+
         self.stay = True
 
     def cut_sheet(self, sheet, columns, rows):
@@ -32,7 +27,13 @@ class Character(pygame.sprite.Sprite):
                 self.frames.append(sheet.subsurface(pygame.Rect(
                     frame_location, self.rect.size)))
 
+    def change_animation(self, sheet, columns, rows):
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+
     def update(self):
+        self.mask = pygame.mask.from_surface(self.image)
         if self.stay:
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[self.cur_frame]
@@ -49,13 +50,14 @@ class Player(pygame.sprite.Sprite):
         self.sheet_revive = evil_wizard_pl['revive']
         self.sheet_attack1 = evil_wizard_pl['attack1']
         self.sheet_attack2 = evil_wizard_pl['attack2']
+        self.sheets = {}
 
         self.rect = pygame.Rect(0, 0, 250, 250)
         self.change_animation(self.sheet_idle, 8, 1)
         self.image = self.frames[self.cur_frame]
 
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
+            TILE_WIDTH * pos_x + 15, TILE_HEIGHT * pos_y + 5)
         self.mask = pygame.mask.from_surface(self.image)
         self.pos_x = pos_x
         self.pos_y = pos_y
@@ -71,28 +73,28 @@ class Player(pygame.sprite.Sprite):
 
     def move(self, movement):
         if movement == 'UP':
-            self.rect.y -= pl_speed
+            self.rect.y -= PL_SPEED
             for wall in walls_group:
                 if pygame.sprite.collide_mask(self, wall):
-                    self.rect.y += pl_speed
+                    self.rect.y += PL_SPEED
                     break
         if movement == 'DOWN':
-            self.rect.y += pl_speed
+            self.rect.y += PL_SPEED
             for wall in walls_group:
                 if pygame.sprite.collide_mask(self, wall):
-                    self.rect.y -= pl_speed
+                    self.rect.y -= PL_SPEED
                     break
         if movement == 'RIGHT':
-            self.rect.x += pl_speed
+            self.rect.x += PL_SPEED
             for wall in walls_group:
                 if pygame.sprite.collide_mask(self, wall):
-                    self.rect.x -= pl_speed
+                    self.rect.x -= PL_SPEED
                     break
         if movement == 'LEFT':
-            self.rect.x -= pl_speed
+            self.rect.x -= PL_SPEED
             for wall in walls_group:
                 if pygame.sprite.collide_mask(self, wall):
-                    self.rect.x += pl_speed
+                    self.rect.x += PL_SPEED
                     break
 
     def attacking(self, pos):
@@ -315,16 +317,21 @@ def start_screen():
 
 
 if __name__ == '__main__':
+    # INITIALIZATION
     pygame.init()
     # pygame.key.set_repeat(200, 70)
-
     size = WIDTH, HEIGHT = 500, 500
     screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Shitty Hack\'n\'Slash')
 
+    # SPRITE SETS
+    # tiles sprites sets
     tile_images = {
         'wall': load_image('box.png'),
         'empty': load_image('grass.png')
     }
+
+    # players sprites sets
     evil_wizard_pl = {
         'idle': load_image('evil_wizard_idle.png'),
         'run': load_image('evil_wizard_run.png'),
@@ -334,33 +341,110 @@ if __name__ == '__main__':
         'death': load_image('evil_wizard_death.png'),
         'revive': load_image('evil_wizard_jump.png')
     }
+    martial_hero_pl = {
+        'idle': load_image('martial_hero_idle.png'),
+        'run': load_image('martial_hero_run.png'),
+        'attack1': load_image('martial_hero_attack1.png'),
+        'attack2': load_image('martial_hero_attack2.png'),
+        'take_hit': load_image('martial_hero_take_hit.png'),
+        'death': load_image('martial_hero_death.png'),
+        'revive': load_image('martial_hero_jump.png')
+    }
+    necromancer_pl = load_image("necromancer_sheet.png")
+    night_borne_pl = load_image("night_borne_sheet.png")
+    players_sets = {
+        'evil_wizard': evil_wizard_pl,
+        'martial_hero': martial_hero_pl,
+        'necromancer': necromancer_pl,
+        'night_borne': night_borne_pl
+    }
 
-    tile_width = tile_height = 50
-    pl_speed = 3
+    # monsters sprites sets
+    goblin_m = {
+        'idle': load_image('goblin_idle.png'),
+        'run': load_image('goblin_run.png'),
+        'take_hit': load_image('goblin_take_hit.png'),
+        'death': load_image('goblin_death.png')
+    }
+    skeleton_m = {
+        'idle': load_image('skeleton_idle.png'),
+        'run': load_image('skeleton_run.png'),
+        'take_hit': load_image('skeleton_take_hit.png'),
+        'death': load_image('skeleton_death.png')
+    }
+    mushroom_m = {
+        'idle': load_image('mushroom_idle.png'),
+        'run': load_image('mushroom_run.png'),
+        'take_hit': load_image('mushroom_take_hit.png'),
+        'death': load_image('mushroom_death.png')
+    }
+    demon_m = {
+        'idle': load_image('demon_idle.png'),
+        'run': load_image('demon_idle.png'),
+        'take_hit': load_image('demon_idle.png'),
+        'death': load_image('demon_idle.png'),
+        'attack': load_image('demon_attack_breath.png')
+    }
+    ghost_m = {
+        'idle': load_image('ghost_idle.png'),
+        'run': load_image('demon_idle.png'),
+        'take_hit': load_image('demon_idle.png'),
+        'death': load_image('demon_idle.png')
+    }
+    trash_monster_m = load_image("trash_monster_sheet.png")
+    monsters_sets = {
+        'goblin': goblin_m,
+        'skeleton': skeleton_m,
+        'mushroom': mushroom_m,
+        'trash_monster': trash_monster_m,
+        'demon': demon_m,
+        'ghost': ghost_m
+    }
 
-    pygame.display.set_caption('Shitty Hack\'n\'Slash')
+    # SPRITE GROUPS
     all_sprites = pygame.sprite.Group()
+
+    # tiles groups
     tiles_group = pygame.sprite.Group()
     walls_group = pygame.sprite.Group()
+
+    # characters groups
+    characters_group = pygame.sprite.Group()
     player_group = pygame.sprite.Group()
+    monsters_group = pygame.sprite.Group()
+
+    # bullets groups
     bullets_group = pygame.sprite.Group()
     pl_bullets_group = pygame.sprite.Group()
     en_bullets_group = pygame.sprite.Group()
 
+    # CONSTANTS
+    TILE_WIDTH = TILE_HEIGHT = 50
+    PL_SPEED = 3
+
+    # time
     FPS = 60
     clock = pygame.time.Clock()
     iteration = 0
+
+    # CAMERA INITIALIZATION
     camera = Camera()
 
+    # LOADING MAP AND PLAYER INITIALIZATION
     level_map = load_level('map2.txt')
     player, level_x, level_y = generate_level(level_map)
-    start_screen()
-    keys = set()
 
+    # MENU
+    start_screen()
+
+    # GAME BEGIN
+    keys = set()
     while True:
         for event in pygame.event.get():
+            # quit checking
             if event.type == pygame.QUIT:
                 terminate()
+            # movement start checking
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP or event.key == pygame.K_w:
                     keys.add('UP')
@@ -381,6 +465,7 @@ if __name__ == '__main__':
                     player.reviving()
                 if event.key == pygame.K_h:
                     player.taking_hit()
+            # movement end checking
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP or event.key == pygame.K_w:
                     keys.discard('UP')
@@ -398,9 +483,11 @@ if __name__ == '__main__':
                     keys.discard('LEFT')
                     if not len(keys) and not player.is_attacking():
                         player.staying()
+            # attack start checking
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     player.attacking(event.pos)
+            # attack end checking
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     player.stop_attack()
@@ -410,12 +497,15 @@ if __name__ == '__main__':
                         player.running()
         camera.update(player)
 
+        # moving if player is alive
         if player.is_alive():
             for k in keys:
                 player.move(k)
+        # camera
         for sprite in all_sprites:
             camera.apply(sprite)
 
+        # iteration and updating
         screen.fill((0, 0, 0))
 
         all_sprites.update()
